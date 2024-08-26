ada1cc973f2ff21fac0c0c9d3176722af45cc2f3
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-08-26 12:44:05 +0200

    Create service provider project incl. CI build

---
+++ customer-api-provider/src/main/java/de/samples/schulung/quarkus/CustomerProviderApplication.java
@@ -0,0 +1,13 @@
+package de.samples.schulung.quarkus;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class CustomerProviderApplication {
+
+    public static void main(String[] args) {
+        Quarkus.run(args);
+    }
+
+}
