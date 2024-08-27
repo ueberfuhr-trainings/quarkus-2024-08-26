2b31a3525bd201d6272e2a97cb378db42d678430
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-08-27 12:39:50 +0200

    Add sample test (failing)

---
+++ customer-api-provider/src/test/java/de/samples/schulung/quarkus/ApiTests.java
@@ -0,0 +1,14 @@
+package de.samples.schulung.quarkus;
+
+import org.junit.jupiter.api.Test;
+
+class ApiTests {
+
+  // GET /customers -> 200 + JSON
+  @Test
+  void should_Return200_When_Get_Customers() {
+throw new IllegalArgumentException();
+  }
+
+
+}
